c1eedd996b4aab7d066d58fb2a28eb4c6ccd75c9
unknown <nahomkeneni4@gmail.com>
2025-06-12 05:41:29 -0700

    updated

--- config/config.go
+++ config/config.go
@@ -1,1 +1,15 @@
 package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func InitCinfig() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("error while loading the .env file")
+	}
+}
