0c48e0d0bfef14c4de405af0cb74f1bc8ebf21bd
unknown <nahomkeneni4@gmail.com>
2025-06-12 05:36:34 -0700

    project_initiated

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
 
+	"os"
+)
+
+func main() {
+	r := gin.Defeault()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Get("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "this is the first APi",
+		})
+	})
+}
